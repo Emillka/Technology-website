bc047ae1d5b827561e12b78eddc379694a9bb60b
Emilka <emilka031916@gmail.com>
2024-01-05 10:45:06 +0100

    website_technology

---
+++ plik.js
@@ -0,0 +1,57 @@
+//var ten = document.getElementById("ten"); =jeden element
+//var $ten = $("#ten");
+//document.querySelectorAll("#ten a"); pobiera element a z div ten
+
+$("h2").on("click",function(){
+    $("body,html").animate({
+        scrollTop: 850 
+    }, 1000)
+})
+
+$("#bio").on("click",function(){
+    $("body,html").animate({
+        scrollTop: 1950 
+    }, 1000)
+})
+
+$("#nano").on("click",function(){
+    $("body,html").animate({
+        scrollTop: 2860 
+    }, 1000)
+})
+
+$("#net").on("click",function(){
+    $("body,html").animate({
+        scrollTop: 3800 
+    }, 1000)
+})
+$("#img1").on("click",function(){
+    $("body,html").animate({
+        scrollTop: 850 
+    }, 1000)
+})
+$("#img2").on("click",function(){
+    $("body,html").animate({
+        scrollTop: 2860
+    }, 1000)
+})
+$("#img3").on("click",function(){
+    $("body,html").animate({
+        scrollTop: 850 
+    }, 1000)
+})
+$("#img4").on("click",function(){
+    $("body,html").animate({
+        scrollTop: 3800
+    }, 1000)
+})
+$(".return").on("click",function(){
+    $("body,html").animate({
+        scrollTop: 850
+    }, 1000)
+})
+
+
+
+
+ 
